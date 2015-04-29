eb57ec6611bf8110ae87ad9a553073f21c17537c
Phil McClure <pmcclure@rumblelabs.com>
2015-04-29 21:25:09 +0100

    Save to tmp then upload to s3

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'newrelic_rpm'
 gem 's3io'
-gem 'rbtrace'
+gem 'rbtrace', require: false, platform: :mri
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/script/heap_dump.rb
+++ app/script/heap_dump.rb
@@ -1,10 +1,14 @@
 require "objspace"
 require 'aws-sdk'
-require 's3io'
 
 ObjectSpace.trace_object_allocations_start
 GC.start()
+
+save_path = Rails.root.join('tmp', 'heap_dump.json')
+
+ObjectSpace.dump_all(output: File.open(save_path, 'w'))
+
 s3 = Aws::S3::Resource.new
-s3_object = s3.bucket(ENV['AWS_BUCKET']).object('heap_dump.json')
-ObjectSpace.dump_all(output: S3io.open(s3_object, 'w'))
+obj = s3.bucket(ENV['AWS_BUCKET']).object('heap_dump.json')
+obj.upload_file(save_path)
 
