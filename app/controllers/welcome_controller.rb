0f1827437ccb4df65366a831718756f48f133e3e
Phil McClure <pmcclure@rumblelabs.com>
2015-04-29 22:05:55 +0100

    Add profile route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+  end
+
+  def profile
     system("rbtrace -p #{Process.pid} -e 'load \"#{Rails.root}/script/heap_dump.rb\"'")
+    render :text => "Profiling complete"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/profile', to: 'welcome#profile'
 end
