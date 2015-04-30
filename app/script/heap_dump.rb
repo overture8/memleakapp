76bb06ae17931ed55a0821d3f437c244045c4b0a
Phil McClure <pmcclure@rumblelabs.com>
2015-04-30 12:07:36 +0100

    Run heap trace in thread and add diff heap script

--- app/script/heap_dump.rb
+++ app/script/heap_dump.rb
@@ -1,15 +1,16 @@
-require 'objspace'
-require 'aws-sdk'
-require 'rbtrace'
+Thread.new {
+  require 'objspace'
+  require 'aws-sdk'
+  require 'rbtrace'
 
-ObjectSpace.trace_object_allocations_start
-GC.start()
+  ObjectSpace.trace_object_allocations_start
+  GC.start()
 
-save_path = Rails.root.join('tmp', 'heap_dump.json')
+  save_path = Rails.root.join('tmp', 'heap_dump.json')
 
-ObjectSpace.dump_all(output: File.open(save_path, 'w'))
+  ObjectSpace.dump_all(output: File.open(save_path, 'w'))
 
-s3 = Aws::S3::Resource.new
-obj = s3.bucket(ENV['AWS_BUCKET']).object('heap_dump.json')
-obj.upload_file(save_path)
-
+  s3 = Aws::S3::Resource.new
+  obj = s3.bucket(ENV['AWS_BUCKET']).object('heap_dump.json')
+  obj.upload_file(save_path)
+}.join
